4981d492af68fced2487caa710fe6bb1d385ae99
Roman Brito <romanbrito1@gmail.com>
2017-04-26 17:16:45 -0500

    initial setup

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// mysql connection
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connection: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+app.set('port', process.env.PORT || 8080);
+
+// Serve static content
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// set handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handelbars');
+
+// require Routes with app.use
+
+ var server = app.listen(app.get('port'), function () {
+     console.log('Listening on port ' + app.get('port'));
+ });
+
